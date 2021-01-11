972aadc5f880e1cc84ed8be401197908cfa2c8ba
Jakalanern <77250250+Jakalanern@users.noreply.github.com>
2021-01-10 17:07:38 -0800

    Add files via upload

---
+++ Age test program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Age_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Okay " + name + ". Are you over 18? Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+            {             
+                Console.WriteLine("You are too young to continue!");
+            }
+            else if (age > 18)
+            {
+                Console.WriteLine("You are over 18!");
+            }
+            else if (age == 18)
+            {
+                Console.WriteLine("You are exactly 18!");
+            }
+            else
+            {
+                Console.WriteLine("Error, error, error");
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
